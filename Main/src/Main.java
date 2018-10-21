3e5fb95f41bbd06f37aa69ee9c10211a8be22494
ilari <ilari@DESKTOP-77EGQRQ>
2018-10-21 20:58:10 +0200

    modified Main.java

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Jyiole was here");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("printing "+i);
+		}
 	}
 
 }
